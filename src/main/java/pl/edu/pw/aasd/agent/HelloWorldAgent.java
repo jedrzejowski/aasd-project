e863de87f605cbd9ac1fa4377235ee2d4cf87944
Adam Jędrzejowski <adam@jedrzejowski.pl>
2020-11-29 22:24:19 +0100

    przejście na jave i dodanie automatycznego budowania

---
+++ src/main/java/pl/edu/pw/aasd/agent/HelloWorldAgent.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.aasd.agent;
+
+import jade.core.Agent;
+
+public class HelloWorldAgent extends Agent {
+
+    protected void setup() {
+        System.out.println("Hello World! My name is " + getLocalName());
+
+        // Make this agent terminate
+        doDelete();
+    }
+}
---
+++ src/main/java/pl/edu/pw/aasd/main.java
@@ -0,0 +1,10 @@
+package pl.edu.pw.aasd;
+
+import pl.edu.pw.aasd.agent.HelloWorldAgent;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(new HelloWorldAgent().isAlive());
+        System.out.println("Hello World!");
+    }
+}
