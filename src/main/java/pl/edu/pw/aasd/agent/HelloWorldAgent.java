15591336a34121ed4416b21ed49f8f51908979ca
Adam Jędrzejowski <adam@jedrzejowski.pl>
2020-12-02 16:46:54 +0100

    dodanie jade.Boot

--- src/main/java/pl/edu/pw/aasd/agent/HelloWorldAgent.java
+++ src/main/java/pl/edu/pw/aasd/agent/HelloWorldAgent.java
@@ -6,8 +6,7 @@ public class HelloWorldAgent extends Agent {
 
     protected void setup() {
         System.out.println("Hello World! My name is " + getLocalName());
-
-        // Make this agent terminate
-        doDelete();
     }
+
+
 }
