e0b50a2d1db6b8f5165e9fb8d674d1e91a9580d5
Adam Jędrzejowski <adam@jedrzejowski.pl>
2020-12-13 23:55:35 +0100

    dodanie reszty agentów

---
+++ src/main/java/pl/edu/pw/aasd/agent/OwnerAgent.java
@@ -0,0 +1,11 @@
+package pl.edu.pw.aasd.agent;
+
+import jade.core.Agent;
+
+public class OwnerAgent extends Agent {
+
+    @Override
+    protected void setup() {
+
+    }
+}
---
+++ src/main/java/pl/edu/pw/aasd/agent/PartnerAgent.java
@@ -0,0 +1,11 @@
+package pl.edu.pw.aasd.agent;
+
+import jade.core.Agent;
+
+public class PartnerAgent extends Agent {
+
+    @Override
+    protected void setup() {
+
+    }
+}
